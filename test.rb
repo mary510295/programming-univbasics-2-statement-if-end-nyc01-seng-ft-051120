76ecee9fa3f9866769cc65bd1452b2732338d085
Fu <fu.goto@gmail.com>
2020-04-20 02:09:26 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,10 +1,19 @@
-if true
-  puts "this code runs!"
-end
+# if true
+#   puts "this code runs!"
+# end
 
-run_code_inside = false
-puts "code before if...end"
-if run_code_inside
-  puts"code inside"
+# run_code_inside = false
+# puts "code before if...end"
+# if run_code_inside
+#   puts"code inside"
+# end
+# puts "code after if...end"
+
+chance_of_rain = 0.2 
+puts "lets go out"
+if chance_of_rain > 0.5
+  puts "umbrella"
+else
+  puts "enjoy"
 end
-puts "code after if...end"
+puts 'either way wear spf'
