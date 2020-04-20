561f49c23638fe0880d11bfa434341a4e9cf7269
Fu <fu.goto@gmail.com>
2020-04-20 02:23:08 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,22 @@
+# if true
+#   puts "this code runs!"
+# end
+
+# run_code_inside = false
+# puts "code before if...end"
+# if run_code_inside
+#   puts"code inside"
+# end
+# puts "code after if...end"
+
+chance_of_rain = 1 
+# puts "lets go out"
+# if chance_of_rain <= 0.25
+#   puts "sun shelter"
+# elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+# puts 'umbrella'
+# else
+#   puts "stay home"
+# end
+# puts 'either way wear spf'
+
